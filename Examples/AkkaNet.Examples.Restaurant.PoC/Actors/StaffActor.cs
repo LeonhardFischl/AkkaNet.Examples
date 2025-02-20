ea6f7a8e1a6e04f236dd1d2f34078ddb5b993af6
Leonhard Fischl <Leonhard.Fischl@EXXETA.com>
2025-02-20 17:20:59 +0100

    Added second part for PoC demonstrations;

---
+++ Examples/AkkaNet.Examples.Restaurant.PoC/Actors/StaffActor.cs
@@ -0,0 +1,28 @@
+﻿using Akka.Actor;
+using AkkaNet.Examples.Restaurant.PoC.Events;
+
+namespace AkkaNet.Examples.Restaurant.PoC.Actors
+{
+	/// <summary>
+	/// Represents a staff member of the restaurant who likes to process the information about customers.
+	/// </summary>
+	public class StaffActor : ReceiveActor
+	{
+		private readonly string _role;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="StaffActor"/> class.
+		/// </summary>
+		/// <param name="role">A barkeeper, cook, waiter or any staff member</param>
+		public StaffActor(string role)
+		{
+			_role = role;
+			Receive<CustomerEnter>(HandleCustomerEnter);
+		}
+
+		private void HandleCustomerEnter(CustomerEnter info)
+		{
+			Console.WriteLine($"[{_role}] Processing information for {info.CustomerName}: Preferences - {string.Join(", ", info.Preferences)}, Needs - {string.Join(", ", info.Needs)}.");
+		}
+	}
+}
--- Examples/AkkaNet.Examples.Restaurant.PoC/Program.cs
+++ Examples/AkkaNet.Examples.Restaurant.PoC/Program.cs
@@ -10,20 +10,73 @@ class Program
 	public static void Main(string[] args)
 	{
 		// Aktorsystem erstellen
-		using var system = ActorSystem.Create("RestaurantSystem");
+		using var system = ActorSystem.Create("restaurantSystem");
+		
+		// Dummy-Daten für Kunden laden - in der realen Welt kommt das aus einer Datenbank oder einem anderen System
+		var customerData = PrepareSpecialGuestList();
 
-		// Akteur erstellen
-		var bouncerActor = system.ActorOf<BouncerActor>("BouncerActor");
+		// Türsteher erstellen
+		var bouncerActor = system.ActorOf(Props.Create(() => new BouncerActor(customerData)), "bouncerActor");
 
-		// Nachrichten an den Akteur senden
-		for (int i = 0; i < 12; i++)
+		// Staff Members erstellen und abonnieren
+		var chef = system.ActorOf(Props.Create(() => new StaffActor("Chef")), "chef");
+		var cook1 = system.ActorOf(Props.Create(() => new StaffActor("Cook1")), "cook1");
+		var cook2 = system.ActorOf(Props.Create(() => new StaffActor("Cook2")), "cook2");
+
+		// Kellner erstellen
+		var waiters = new List<IActorRef>();
+		for (int i = 1; i <= 5; i++)
 		{
-			bouncerActor.Tell(new CustomerEnter());
+			waiters.Add(system.ActorOf(Props.Create(() => new StaffActor($"Waiter{i}")), $"waiter{i}"));
 		}
 
-		bouncerActor.Tell(new CustomerExit());
+		// Barkeeper erstellen
+		var barkeeper1 = system.ActorOf(Props.Create(() => new StaffActor("Barkeeper1")), "barkeeper1");
+		var barkeeper2 = system.ActorOf(Props.Create(() => new StaffActor("Barkeeper2")), "barkeeper2");
+
+		// Abonnieren der Türsteher-Nachrichten
+		system.EventStream.Subscribe(chef, typeof(CustomerEnter));
+		system.EventStream.Subscribe(cook1, typeof(CustomerEnter));
+		system.EventStream.Subscribe(cook2, typeof(CustomerEnter));
+		foreach (var waiter in waiters)
+		{
+			system.EventStream.Subscribe(waiter, typeof(CustomerEnter));
+		}
+		system.EventStream.Subscribe(barkeeper1, typeof(CustomerEnter));
+		system.EventStream.Subscribe(barkeeper2, typeof(CustomerEnter));
+
+		for (int i = 0; i < 12; i++)
+		{
+			bouncerActor.Tell(customerData.ElementAt(i).Key);
+		}
+		
+		bouncerActor.Tell(new CustomerExit("Michael"));
+
+		bouncerActor.Tell(new CustomerExit("Emma"));
 
 		// Warte, damit die Ausgabe sichtbar bleibt
 		Console.ReadLine();
+	}
+
+	private static Dictionary<string, (List<CustomerPreferences>, List<CustomerNeeds>)> PrepareSpecialGuestList()
+	{
+		// Dummy-Daten für Kunden - in der realen Welt kommt das aus einer Datenbank oder einem anderen System
+		var customerNames = new[] { "Alice", "John", "Jane", "Michael", "Bob", "Sarah", "David", "Emma", "Thomas", "Chris", "Olivia", "Daniel" };
+		var random = new Random();
+		var customerPreferences = Enum.GetValues(typeof(CustomerPreferences)).Cast<CustomerPreferences>().ToArray();
+		var customerNeeds = Enum.GetValues(typeof(CustomerNeeds)).Cast<CustomerNeeds>().ToArray();
+
+		var dict = new Dictionary<string, (List<CustomerPreferences>, List<CustomerNeeds>)>();
+		for (int i = 0; i < 12; i++)
+		{
+			var randomPreferences = new List<CustomerPreferences>();
+			var randomNeeds = new List<CustomerNeeds>();
+
+			randomPreferences.Add(customerPreferences[random.Next(customerPreferences.Length)]);
+			randomNeeds.Add(customerNeeds[random.Next(customerNeeds.Length)]);
+
+			dict.Add(customerNames[i], (randomPreferences, randomNeeds));
+		}
+		return dict;
 	}
 }
