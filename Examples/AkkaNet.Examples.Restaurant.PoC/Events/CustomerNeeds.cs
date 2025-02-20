1cf64dec5d8791a3b5dd30e11efea1ce12038883
Leonhard Fischl <Leonhard.Fischl@EXXETA.com>
2025-02-20 17:20:59 +0100

    Added second part for PoC demonstrations;

---
+++ Examples/AkkaNet.Examples.Restaurant.PoC/Events/CustomerNeeds.cs
@@ -0,0 +1,39 @@
+﻿namespace AkkaNet.Examples.Restaurant.PoC.Events
+{
+	/// <summary>
+	/// Represents the needs of a customer as example data.
+	/// </summary>
+	public enum CustomerNeeds
+    {
+		/// <summary>
+		/// The customer's needs are unknown.
+		/// </summary>
+		Unbekannt,
+
+		/// <summary>
+		/// The customer has a strong peanut allergy.
+		/// </summary>
+		StarkeErdnussallergie,
+
+		/// <summary>
+		/// The customer has a tomato intolerance.
+		/// </summary>
+		TomatenUnverträglichkeit,
+
+		/// <summary>
+		/// The customer wants to sit at the window side.
+		/// </summary>
+		Fensterplatz,
+
+		/// <summary>
+		/// The customer wants not to sit near the restroom.
+		/// </summary>
+		NichtNebenDerToilette,
+
+		/// <summary>
+		/// The customer wants to sit in a quiet area.
+		/// </summary>
+		RuheBereich
+	}
+
+}
---
+++ Examples/AkkaNet.Examples.Restaurant.PoC/Events/CustomerPreferences.cs
@@ -0,0 +1,34 @@
+﻿namespace AkkaNet.Examples.Restaurant.PoC.Events
+{
+    public enum CustomerPreferences
+    {
+		/// <summary>
+		/// The customer's preferences are unknown.
+		/// </summary>
+		Unbekannt,
+		/// <summary>
+		/// The customer prefers wine.
+		/// </summary>
+		Weinkarte,
+		/// <summary>
+		/// The customer prefers espresso.
+		/// </summary>
+		Espresso,
+		/// <summary>
+		/// The customer prefers a schnaps after the meal.
+		/// </summary>
+		Schnaps,
+		/// <summary>
+		/// The customer prefers a vegetarian food.
+		/// </summary>
+		Vegetarisch,
+		/// <summary>
+		/// The customer prefers a vegan food.
+		/// </summary>
+		Vegan,
+		/// <summary>
+		/// The customer prefers a gluten-free food.
+		/// </summary>
+		GlutenFrei,
+	}
+}
