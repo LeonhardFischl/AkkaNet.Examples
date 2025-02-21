8c4f8ea80007c7c237843b76b8153fd7a450916d
Leonhard Fischl <Leonhard.Fischl@EXXETA.com>
2025-02-21 18:27:30 +0100

    Added new advanced example for an asp.net core web api including a controller which sends the incoming message to the actor system;

---
+++ Examples/Akka.Net.Examples.Restaurant.PoC.Api/Actors/BouncerActor.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using Akka.Net.Examples.Restaurant.PoC.Api.Events;
+using static Akka.IO.Tcp;
+
+namespace Akka.Net.Examples.Restaurant.PoC.Api.Actors;
+
+public class BouncerActor : ReceiveActor
+{
+	private readonly IActorRef _chiefOfWaiter;
+	private readonly IActorRef[] _waiters;
+
+	public BouncerActor(IActorRef chiefOfWaiter, IActorRef[] waiters)
+	{
+		_chiefOfWaiter = chiefOfWaiter;
+		_waiters = waiters;
+
+		Receive<CustomerArrived>(msg =>
+		{
+			if (msg.Customer.Recognized)
+			{
+				_chiefOfWaiter.Tell(new GuideCustomer(msg.Customer));
+			}
+			else
+			{
+				foreach (var waiter in _waiters)
+				{
+					waiter.Tell(new GuideCustomer(msg.Customer));
+				}
+			}
+		});
+	}
+}
---
+++ Examples/Akka.Net.Examples.Restaurant.PoC.Api/Events/CustomerEvents.cs
@@ -0,0 +1,27 @@
+﻿namespace Akka.Net.Examples.Restaurant.PoC.Api.Events;
+
+public class Customer
+{
+	public string Name { get; set; }
+	public bool Recognized { get; set; }  // Simple flag to determine if the customer is recognized
+}
+
+public class CustomerArrived
+{
+	public Customer Customer { get; }
+
+	public CustomerArrived(Customer customer)
+	{
+		Customer = customer;
+	}
+}
+
+public class GuideCustomer
+{
+	public Customer Customer { get; }
+
+	public GuideCustomer(Customer customer)
+	{
+		Customer = customer;
+	}
+}
