91d6c61eb3c92b882731aef4d3a124345761d99c
Leonhard Fischl <Leonhard.Fischl@EXXETA.com>
2025-02-21 18:27:30 +0100

    Added new advanced example for an asp.net core web api including a controller which sends the incoming message to the actor system;

---
+++ Examples/Akka.Net.Examples.Restaurant.PoC.Api/Actors/WaiterActor.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+using Akka.Net.Examples.Restaurant.PoC.Api.Events;
+
+namespace Akka.Net.Examples.Restaurant.PoC.Api.Actors;
+
+public class WaiterActor : ReceiveActor
+{
+	public WaiterActor()
+	{
+		Receive<GuideCustomer>(msg =>
+		{
+			// Logic to guide the customer to a table
+			Console.WriteLine($"Guiding {msg.Customer.Name} to a table.");
+		});
+	}
+}
---
+++ Examples/Akka.Net.Examples.Restaurant.PoC.Api/Program.cs
@@ -0,0 +1,19 @@
+
+using Akka.Actor;
+using Akka.Hosting;
+
+namespace Akka.Net.Examples.Restaurant.PoC.Api;
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		CreateHostBuilder(args).Build().Run();
+	}
+
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+		Host.CreateDefaultBuilder(args)
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+				webBuilder.UseStartup<Startup>();
+			});
+}
