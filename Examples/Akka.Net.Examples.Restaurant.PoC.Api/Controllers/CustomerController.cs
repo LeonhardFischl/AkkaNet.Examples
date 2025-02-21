bc09c7f267321ced755f8632b4a05519b80656c4
Leonhard Fischl <Leonhard.Fischl@EXXETA.com>
2025-02-21 18:27:30 +0100

    Added new advanced example for an asp.net core web api including a controller which sends the incoming message to the actor system;

---
+++ Examples/Akka.Net.Examples.Restaurant.PoC.Api/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Akka.Actor;
+using Akka.Hosting;
+using Akka.Net.Examples.Restaurant.PoC.Api.Actors;
+using Akka.Net.Examples.Restaurant.PoC.Api.Events;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Akka.Net.Examples.Restaurant.PoC.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CustomerController : ControllerBase
+{
+	private readonly IActorRef _bouncer;
+
+	public CustomerController(IRequiredActor<BouncerActor> bouncerActor)
+	{
+		_bouncer = bouncerActor.ActorRef;
+	}
+
+	[HttpPost("arrive")]
+	public IActionResult CustomerArrives([FromBody] Customer customer)
+	{
+		_bouncer.Tell(new CustomerArrived(customer));
+		return Ok($"Customer {customer.Name} processed.");
+	}
+}
---
+++ Examples/Akka.Net.Examples.Restaurant.PoC.Api/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Akka.Actor;
+using Akka.Hosting;
+using Akka.Net.Examples.Restaurant.PoC.Api.Actors;
+
+namespace Akka.Net.Examples.Restaurant.PoC.Api;
+
+public class Startup
+{
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddControllers();
+
+		services.AddAkka("RestaurantSystem", (configurationBuilder, provider) =>
+		{
+			// configurationBuilder.AddHoconFile(... ) // Add configuration file
+			// configurationBuilder.WithCluster(... ) // Needs Akka.Cluster dependency
+
+			configurationBuilder.WithActors((system, registry, _) =>
+			{
+				var chiefOfWaiter = system.ActorOf<ChiefOfWaiterActor>("chiefOfWaiter");
+				var waiters = new IActorRef[]
+				{
+					system.ActorOf<WaiterActor>("waiter1"),
+					system.ActorOf<WaiterActor>("waiter2"),
+					system.ActorOf<WaiterActor>("waiter3")
+				};
+
+				var bouncerActor = system.ActorOf(Props.Create(() => new BouncerActor(chiefOfWaiter, waiters)), "bouncer");
+				registry.Register<BouncerActor>(bouncerActor);
+			});
+		});
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if (env.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
+
+		app.UseRouting();
+
+		app.UseEndpoints(endpoints =>
+		{
+			endpoints.MapControllers();
+		});
+	}
+}
