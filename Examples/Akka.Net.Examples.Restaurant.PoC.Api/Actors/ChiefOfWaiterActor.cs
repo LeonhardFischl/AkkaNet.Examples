8dc3d4f152b46d52b7a6be5bf3f334974331b611
Leonhard Fischl <Leonhard.Fischl@EXXETA.com>
2025-02-21 18:27:30 +0100

    Added new advanced example for an asp.net core web api including a controller which sends the incoming message to the actor system;

---
+++ Examples/Akka.Net.Examples.Restaurant.PoC.Api/Actors/ChiefOfWaiterActor.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+using Akka.Net.Examples.Restaurant.PoC.Api.Events;
+
+namespace Akka.Net.Examples.Restaurant.PoC.Api.Actors;
+
+public class ChiefOfWaiterActor : ReceiveActor
+{
+	public ChiefOfWaiterActor()
+	{
+		Receive<GuideCustomer>(msg =>
+		{
+			// Logic to guide the customer to a quiet area
+			Console.WriteLine($"Guiding {msg.Customer.Name} to a quiet area.");
+		});
+	}
+}
