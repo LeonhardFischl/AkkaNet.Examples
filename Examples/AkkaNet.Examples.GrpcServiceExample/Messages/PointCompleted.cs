ee3a23fa461fdf2711c88a5ac2073bee039b1758
Leonhard Fischl <Leonhard.Fischl@EXXETA.com>
2025-06-28 12:15:12 +0200

    Added missing PointCompleted for statistics after and during batch processing;

---
+++ Examples/AkkaNet.Examples.GrpcServiceExample/Messages/PointCompleted.cs
@@ -0,0 +1,7 @@
+﻿namespace AkkaNet.Examples.GrpcServiceExample.Messages;
+
+public sealed class PointCompleted(string batchId)
+{
+	public string BatchId { get; } = batchId;
+	public DateTime CompletedAt { get; } = DateTime.UtcNow;
+}
